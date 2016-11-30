d2fe4f009a78d606177c6012c51c19bef80b9ce6
Ron Calderon <Ron.calderon@nbcuni.com>
2016-11-30 08:24:47 -0500

    removed unused imports more cleanup add snakeyaml import disabled spring banner

--- src/main/groovy/com/webops/jenkins/spring/Meow.groovy
+++ src/main/groovy/com/webops/jenkins/spring/Meow.groovy
@@ -4,12 +4,11 @@ import com.webops.jenkins.spring.config.GitHubProperties
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.groovy.template.GroovyTemplateAutoConfiguration
 import org.springframework.boot.context.properties.EnableConfigurationProperties
-import org.springframework.boot.context.properties.EnableConfigurationPropertiesImportSelector
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.autoconfigure.web.*
+import org.yaml.snakeyaml.Yaml
 
 @Configuration
 @EnableAutoConfiguration(exclude = [ WebMvcAutoConfiguration.class, WebClientAutoConfiguration.class,
@@ -20,7 +19,8 @@ public class Meow {
   final Map<String, String> DEFAULT_PROPS = [
     'spring.config.location' : 'resources',
     'spring.application.name': 'meow',
-    'spring.config.name'     : 'default,${spring.application.name}'
+    'spring.main.banner-mode' : 'off',
+    'spring.config.name'     : 'application,${spring.application.name}'
   ]
 
   void main(String[] args) {
@@ -30,10 +30,5 @@ public class Meow {
         .run(args);
     println "started: ${System.getProperty('user.home')} "
   }
-
-  //@Override
- // SpringApplicationBuilder configure(SpringApplicationBuilder application) {
- //   application.properties(DEFAULT_PROPS).sources(Main)
- // }
 
 }
