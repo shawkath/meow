2efaa5fb61494def4438957193c40f6f33bdb1d7
Shawkath Khan <RLE0221@rlpl.com>
2016-11-30 12:33:05 -0500

    added lib

--- src/main/groovy/com/webops/jenkins/spring/config/FolderConfig.groovy
+++ src/main/groovy/com/webops/jenkins/spring/config/FolderConfig.groovy
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import javaposse.jobdsl.dsl
+import javaposse.jobdsl.dsl.DslFactory.*
 
 @Configuration
 @ConditionalOnProperty('project.name')
@@ -14,7 +14,7 @@ class FolderConfig {
   @Bean
   void createFolder(FolderProperties folderProperties) {
     folder("${folderProperties.name}") {
-      primaryView(folderProperties.primaryView)
+      //primaryView(folderProperties.primaryView)
     }
   }
 }
