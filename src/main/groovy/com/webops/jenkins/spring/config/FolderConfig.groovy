8f517cfe2e48325399e1963c85e47fbec080fa00
Ron Calderon <Ron.calderon@nbcuni.com>
2016-11-30 13:45:38 -0500

    updated to use enabled flag

--- src/main/groovy/com/webops/jenkins/spring/config/FolderConfig.groovy
+++ src/main/groovy/com/webops/jenkins/spring/config/FolderConfig.groovy
@@ -4,15 +4,16 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import javaposse.jobdsl.dsl.DslFactory.*
+//import javaposse.jobdsl.dsl.DslFactory.*
 
 @Configuration
-@ConditionalOnProperty('project.name')
+@ConditionalOnProperty('project.enabled')
 @EnableConfigurationProperties(FolderProperties)
 class FolderConfig {
 
   @Bean
   void createFolder(FolderProperties folderProperties) {
+    println "meow"
     folder("${folderProperties.name}") {
       //primaryView(folderProperties.primaryView)
     }
